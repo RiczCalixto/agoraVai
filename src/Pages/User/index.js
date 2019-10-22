15fae61217bf4717f08b3d9715937847ee1dcec4
Ricardo Calixto <rcalixtos@gmail.com>
2019-10-21 21:49:03 -0300

    Fetching more data from API after getting all starred repositories from page

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {ActivityIndicator} from 'react-native';
+
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 import {
   Container,
@@ -13,6 +14,7 @@ import {
   Info,
   Title,
   Author,
+  Loading,
 } from './styles';
 
 export default class User extends Component {
@@ -20,18 +22,42 @@ export default class User extends Component {
     title: navigation.getParam('user').name,
   });
 
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     stars: [],
-    loading: false,
+    loading: true,
+    page: 1,
   };
 
   async componentDidMount() {
-    const {navigation} = this.props;
-    const user = navigation.getParam('user');
-    this.setState({loading: true});
-    const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({stars: response.data, loading: false});
+    this.load();
   }
+
+  load = async (page = 1) => {
+    const {navigation} = this.props;
+    const {stars} = this.state;
+    const user = navigation.getParam('user');
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: {page},
+    });
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+    });
+  };
+
+  loadMore = () => {
+    const {page} = this.state;
+    const nextPage = page + 1;
+    this.load(nextPage);
+  };
 
   render() {
     const {navigation} = this.props;
@@ -46,11 +72,13 @@ export default class User extends Component {
           <Bio>{user.bio}</Bio>
         </Header>
         {loading ? (
-          <ActivityIndicator />
+          <Loading />
         ) : (
           <Stars
             data={stars}
             keyExtractor={star => String(star.id)}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             renderItem={({item}) => (
               <Starred>
                 <OwnerAvatar source={{uri: item.owner.avatar_url}} />
